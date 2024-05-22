c8a8c17b700b3166f631a2f78c480f3308051277
Vi Hoang <viviethoang9@gmail.com>
2024-05-23 04:15:42 +0700

    Built web
    Branch: upload_raw
    Commit: 64874f21ec92b57663eca64e5aed3fe84c328b26 (dirty)
    
    package:peanut 5.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -9,8 +9,25 @@ if (!window._flutter) {
 _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e31a","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
+// Manipulate the DOM to add a loading spinner which is rendered with this HTML:
+// <div class="loading">
+//   <div class="loader" />
+// </div>
+const loadingDiv = document.createElement('div');
+loadingDiv.className = "loading";
+document.body.appendChild(loadingDiv);
+const loaderDiv = document.createElement('div');
+loaderDiv.className = "loader";
+loadingDiv.appendChild(loaderDiv);
+
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "589976815"
+  onEntrypointLoaded: async function(engineInitializer) {
+    const appRunner = await engineInitializer.initializeEngine();
+
+    // Remove the loading spinner when the app runner is ready
+    if (document.body.contains(loadingDiv)) {
+      document.body.removeChild(loadingDiv);
+    }
+    await appRunner.runApp();
   }
 });
